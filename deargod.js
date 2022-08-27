fe6f57f1a6cb588e9cee9c3b7dc2ec4cb38cf8c7
Albus Fu <b123963@yahoo.com.tw>
2022-08-27 12:48:00 +0800

    一個頁面渲染成功

--- deargod.js
+++ deargod.js
@@ -1,39 +1,20 @@
-function stringToUnicode(stringCode) {
-    var start = 0, end = 0, codeId, newWord, tillNotFound = "True";
-    // 進入迴圈 沒找到 "\\u" 跳出迴圈
-    while (tillNotFound) {
-        // 預先判斷有沒有找到文字
-        tillNotFound = stringCode.includes("\\u", end);
-        // stringCode 找 "\\u" 後面的 codeId
-        start = stringCode.indexOf("\\u");
-        end = start + "\\u".length + 4;
-        codeId = stringCode.slice(start + "\\u".length, end);
-        // 有找到 "\\u"；沒有則跳過
-        // 轉換成新的文字
-        newWord = String.fromCodePoint(parseInt(codeId, 16));
-        // stringCode += 前面的字串 + 新的文字 + 後面的字串
-        stringCode = stringCode.slice(0, start) + newWord + stringCode.slice(end);
-    }
-    // 回傳 stringCode
-    return stringCode;
-}
-
-
-// 依組歸類
-var personList,
-    personByGroup = document.querySelectorAll(".intro_person > div");
 window.onload = function () {
-    var url = "./deargod.json"/*json文件url，本地的就写本地的位置，如果是服务器的就写服务器的路径*/
-    var request = new XMLHttpRequest();
-    request.open("get", url);/*设置请求方法与路径*/
-    request.send(null);/*不发送数据到服务器*/
+    const url = "./people.json"
+    const request = new XMLHttpRequest()
+    request.open("get", url)/*设置请求方法与路径*/
+    request.send(null)/*不发送数据到服务器*/
+
     request.onload = function () {/*XHR对象获取到返回信息后执行*/
         if (request.status == 200) {/*返回状态为200，即为数据获取成功*/
-            personList = JSON.parse(request.responseText);
-            var idNumber;
+            const personList = JSON.parse(request.responseText)
+                , introPerson = document.querySelectorAll(".intro_person")[0]
+
+            console.log(personList)
+            console.log(introPerson)
+
+            let idNumber = 0;
             for (let i = 0; i < personList.length; i++) {
                 for (var j = 0; j < personList[i].length; j++) {
-                    var name = stringToUnicode(personList[i][j]["name"]);
                     var img = personList[i][j]["img"];
                     var title = stringToUnicode(personList[i][j]["title"]);
                     var text = stringToUnicode(personList[i][j]["text"]);
@@ -49,6 +30,56 @@ window.onload = function () {
                     idNumber++;
                 }
             }
+
+            // 建立一個物件依組別存取
+            const peopleDataByGroup = {}
+
+            // 一個函式
+            // if (peopleDataByGroup[group])
+            // false: 在資料內建立資料
+            // 透過資料建立 element
+            function buildPersonElementByGroup(group) {
+                if (!peopleDataByGroup.hasOwnProperty(group)) {
+                    buildGroupData(group)
+                }
+
+                introPerson.innerHTML = ''
+
+                console.log(peopleDataByGroup[group])
+
+                for (const [index, person] of peopleDataByGroup[group].entries()) {
+                    introPerson.innerHTML += `
+                    <div class="intro_who">
+                        <div class="intro_basic" data-person-id=${index}>
+                            <img class="img_round" src=${person.imgUrl} alt="">
+                            <p class="intro_person_name">${person.titleAndName}</p>
+                        </div>
+                        <p class="intro_text">${person.describe}</p>
+                    </div>`
+                }
+            }
+
+            function buildGroupData(group) {
+                const deleteIndex = []
+                peopleDataByGroup[group] = []
+
+                for (const [index, value] of personList.entries()) {
+
+                    if (value.group === group) {
+                        peopleDataByGroup[group].push(value)
+                        deleteIndex.push(index)
+                    }
+                }
+
+                for (i of deleteIndex) {
+                    personList.splice(i, 1)
+                }
+
+                console.log(personList)
+                console.log(peopleDataByGroup)
+            }
+
+            buildPersonElementByGroup('導表組')
 
             // 按鈕點擊事件
             $(".intro_group > input").click(function () {
